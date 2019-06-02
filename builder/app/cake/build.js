6aea417a5f210b83a6458c9174ee201c2037b396
Andrew Porter <mephisto83@hotmail.com>
2019-06-02 12:39:39 -0500

    Update build.js

--- builder/app/cake/build.js
+++ builder/app/cake/build.js
@@ -2,6 +2,7 @@
 
 var fs = require('fs');
 // import path from 'path';
+var path = require('path');
 var child_process = require('child_process'),
     exec = child_process.exec,
     spawn = child_process.spawn;
@@ -22,7 +23,7 @@ function executeSpawnCmd(cmd, args, options) {
             child.kill();
         };
         child.stdout.on('data', function (data) {
-            console.log('stdout: ' + data);
+            // console.log('stdout: ' + data);
         });
 
         child.stderr.on('data', function (data) {
@@ -59,20 +60,46 @@ for (let j = 0; j < process.argv.length; j++) {
 function createWorkSpace() {
     var build = fs.readFileSync('./workspace.json', 'utf-8');
     build = JSON.parse(build);
+    var solutionPath = path.resolve('./' + build.solutionName + '.sln');
     return Promise.resolve().then(() => {
-        if (fs.existsSync('./' + build.solutionName + '.sln')) {
-            return executeSpawnCmd('dotnet', ['new', 'sln', '-n', build.solutionName + '.sln'], {});
+        if (!fs.existsSync('' + build.solutionName + '')) {
+            return executeSpawnCmd('dotnet', ['new', 'sln', '--force', '-n', build.solutionName], {});
         }
     }).then(() => {
-        return executeSpawnCmd('dotnet', ['new', 'mstest', '-n', build.solutionName + '.Tests'], {});
+        return executeSpawnCmd('dotnet', ['new', 'mstest', '--force', '-n', build.solutionName + '.Tests'], {});
     }).then(() => {
-        return executeSpawnCmd('dotnet', ['new', 'classlib', '-n', build.solutionName + '.Interfaces'], {});
+        return executeSpawnCmd('dotnet', ['new', 'webapi', '--force', '-n', build.solutionName + '.Web'], {});
     }).then(() => {
-        return executeSpawnCmd('dotnet'['new', 'classlib', '-n', `"${build.solutionName + '.Models'}"`], {});
+        return executeSpawnCmd('dotnet', ['new', 'classlib', '--force', '-n', build.solutionName + '.Models'], {});
     }).then(() => {
-        return executeSpawnCmd('dotnet', ['new', 'classlib', '-n', build.solutionName + '.Controllers'], {});
+        return executeSpawnCmd('dotnet', ['new', 'classlib', '--force', '-n', build.solutionName + '.Interfaces'], {});
     }).then(() => {
-        return executeSpawnCmd('dotnet', ['new', 'web', '-n', build.solutionName + '.Web'], {});
+        return executeSpawnCmd('dotnet', ['new', 'classlib', '--force', '-n', build.solutionName + '.Controllers'], {});
+    }).then(() => {
+        var projectPath = build.solutionName + '.Tests/' + build.solutionName + '.Tests.csproj';
+        //dotnet sln todo.sln add todo-app/todo-app.csproj
+        return executeSpawnCmd('dotnet', ['sln', solutionPath, 'add', projectPath], {});
+
+    }).then(() => {
+        var projectPath = build.solutionName + '.Web/' + build.solutionName + '.Web.csproj';
+        //dotnet sln todo.sln add todo-app/todo-app.csproj
+        return executeSpawnCmd('dotnet', ['sln', solutionPath, 'add', projectPath], {});
+
+    }).then(() => {
+        var projectPath = build.solutionName + '.Models/' + build.solutionName + '.Models.csproj';
+        //dotnet sln todo.sln add todo-app/todo-app.csproj
+        return executeSpawnCmd('dotnet', ['sln', solutionPath, 'add', projectPath], {});
+
+    }).then(() => {
+        var projectPath = build.solutionName + '.Interfaces/' + build.solutionName + '.Interfaces.csproj';
+        //dotnet sln todo.sln add todo-app/todo-app.csproj
+        return executeSpawnCmd('dotnet', ['sln', solutionPath, 'add', projectPath], {});
+
+    }).then(() => {
+        var projectPath = build.solutionName + '.Controllers/' + build.solutionName + '.Controllers.csproj';
+        //dotnet sln todo.sln add todo-app/todo-app.csproj
+        return executeSpawnCmd('dotnet', ['sln', solutionPath, 'add', projectPath], {});
+
     }).catch(e => {
         console.log(e);
     });
