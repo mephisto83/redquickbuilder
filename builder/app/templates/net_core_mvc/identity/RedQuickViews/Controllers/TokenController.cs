7f1a5b0e58e1cb8d28b3a3ee6fea90ef0ca1e33a
Andrew Porter <mephisto83@hotmail.com>
2019-06-23 21:05:47 -0500

    fds

---
+++ builder/app/templates/net_core_mvc/identity/RedQuickViews/Controllers/TokenController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using RedQuick.Data;
+using RedQuick.Util;
+using RedQuickCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using RedQuickCore.Identity;
+using RedQuickCore.Interfaces;
+using {{namespace}}.Models;
+
+namespace {{namespace}}.Controllers
+{
+    //[Route("auth")]
+    public class TokenController : RedTokenController<{{model}}> 
+    {   
+        public TokenController(
+          UserManager<{{model}}> userManager) :
+            base(userManager)
+        {
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public Task<IActionResult> GenerateToken(){
+            return base.GenerateToken();
+        }
+
+        public override IList<Claim> GetUserClaims(User user)
+        {
+            return new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim("Customer", user.Customer),
+                new Claim(ClaimTypes.NameIdentifier, user.Id)
+            };
+        }
+    }
+}
