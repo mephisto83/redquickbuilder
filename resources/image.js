99c1ccfd941c6f33c28b3b5ac2cf021bffeb1226
Andrew Porter <mephisto83@hotmail.com>
2019-09-12 19:56:31 -0500

    updates

--- resources/image.js
+++ resources/image.js
@@ -8,4 +8,6 @@ console.log(files);
 
 var json = files.map(t => ({ img: 'http://192.168.1.147:8080/images/' + t }));
 
-fs.writeFileSync('../builder/app/data/images.json', JSON.stringify(json));
+fs.writeFileSync('../builder/app/data/images.json', JSON.stringify({
+    conversations: json
+}));
