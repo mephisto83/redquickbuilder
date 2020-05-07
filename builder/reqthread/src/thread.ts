5db54241b67e99d2563e006f938898739ea6d3d9
Andrew Porter <mephisto83@hotmail.com>
2020-05-07 10:23:40 -0500

    Update thread.ts

--- builder/reqthread/src/thread.ts
+++ builder/reqthread/src/thread.ts
@@ -97,9 +97,12 @@ async function loop() {
 	let noerror = true;
 	do {
 		try {
-		 console.log('taking a nap');
-				await sleep(3 * 1000);
-
+			// console.log('taking a nap');
+			if (context.busy) {
+				await sleep(30 * 1000 + Math.random() * 100 * 1000);
+			} else {
+				await sleep(3 * 1000 + Math.random() * 100 * 1000);
+			}
 			if (!threadManagement.ready) {
 				if (context.config) console.log('not ready ' + context.config.agentProject);
 				continue;
@@ -107,29 +110,8 @@ async function loop() {
 			if (context.jobArgs) {
 				let jobArgs = context.jobArgs;
 				context.jobArgs = null;
-				await job(jobArgs);
 				await sleep(10 * 1000);
-			} else {
-				// console.log('loop done ' + context.config.agentProject);
-				if (!context.busy) {
-					// console.log('not busy' + context.config.agentProject);
-
-					if (!context.jobArgs) {
-						if (!context.sinceLastReady || context.sinceLastReady % 1000) {
-							threadManagement.send({
-								response: RedQuickDistributionCommand.RaisingAgentProjectReady,
-								command: RedQuickDistributionCommand.RaisingAgentProjectReady,
-								changed: true,
-								ready: true
-							});
-							context.sinceLastReady = 0;
-						}
-						context.sinceLastReady = context.sinceLastReady || 0;
-						context.sinceLastReady++;
-					}
-				} else {
-					console.log('------------ is busy -----------------');
-				}
+				await job(jobArgs);
 			}
 			let { options } = context;
 			if (options) {
@@ -140,6 +122,21 @@ async function loop() {
 				}
 			} else {
 				console.warn('no options yet');
+			}
+			// console.log('loop done ' + context.config.agentProject);
+			if (!context.busy) {
+				// console.log('not busy' + context.config.agentProject);
+
+				if (!context.jobArgs) {
+					threadManagement.send({
+						response: RedQuickDistributionCommand.RaisingAgentProjectReady,
+						command: RedQuickDistributionCommand.RaisingAgentProjectReady,
+						changed: true,
+						ready: true
+					});
+				}
+			} else {
+				console.log('------------ is busy -----------------');
 			}
 		} catch (e) {
 			noerror = false;
