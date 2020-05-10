9cbe9072822d4dd1f76d52797327d93c9dc840de
mephisto83 <mephisto8335@gmail.com>
2020-05-10 03:26:34 -0500

    tyt

--- builder/reqthread/worker.js
+++ builder/reqthread/worker.js
@@ -15,7 +15,7 @@ let distribution = new Distribution();
     baseFolder: dir,
     workingDirectory:dir,
     throttle: 120 * 1000,
-    threads: 1,
+    threads: adata.threads || 1,
     remoteServerHost: '192.168.1.146',
     remoteServerPort: 7979
   })
